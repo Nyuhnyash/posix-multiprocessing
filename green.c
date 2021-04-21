94918d3ed868d3d0ad59c49940e81949f7f92da6
Nyuhnyash <git@mailing.cf>
2021-04-21 23:36:45 +0400

    Special action through FIFO

--- green.c
+++ green.c
@@ -1,9 +1,19 @@
 #define NAME "GREEN"
 
 #include "core.h"
+#include "lib/fifo.h"
+#include "lib/sem.h"
+#include "pathnames.h"
+
+#define MAX_LINES 32
 
 void green()
 {
 	greeting(NAME);
+
+	int lengths[MAX_LINES] = { 0 };
+	int count = print_swapped_XY(YELLOW_GREEN_FIFO_PATH, lengths);
+	if (count > MAX_LINES) 
+		err("Input file is too long");
 	bye(NAME);
 }
--- yellow.c
+++ yellow.c
@@ -1,14 +1,22 @@
 #define NAME "YELLOW"
 
 #include <sys/wait.h>
+
 #include "core.h"
+#include "lib/fifo.h"
+#include "pathnames.h"
 
 void yellow()
 {
+	create_fifo(YELLOW_GREEN_FIFO_PATH);
+
 	int green_exit_code;
 	pid_t green_pid = create(green, green_exit_code);
 
 	greeting(NAME);
+	
+	print_unchanged(YELLOW_GREEN_FIFO_PATH);
+	
 	wait(&green_exit_code);
 	bye(NAME);
 }
